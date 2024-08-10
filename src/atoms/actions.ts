7cf2fe53c8cdab9d0cf9b265ac2a64f3d6c4c018
Nguyen Duc Thang <103989353+GeniusVenus@users.noreply.github.com>
2024-08-11 00:52:24 +0700

    :hammer: fix: update eslint config

--- src/atoms/actions.ts
+++ src/atoms/actions.ts
@@ -1,4 +1,6 @@
-export const updateUsername = (state: any, value: string) => {
+const updateUsername = (state: any, value: string) => {
   state.set(value);
   return [...state];
 };
+
+export default updateUsername;
