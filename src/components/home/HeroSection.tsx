2a8a423d6a9b163f7bb999977bac406f1e5b9fa8
Nguyen Duc Thang <103989353+GeniusVenus@users.noreply.github.com>
2024-09-24 07:41:04 +0700

    :hammer:  Fix: fix responsive UI (#3)
    * :sparkles: Feat: Home landing page
    
    * :sparkles: Feat: Responsive design for landing page & update based on comments
    
    * :hammer: fix: update responsive UI

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -6,23 +6,25 @@ function HeroSection() {
     <div className="layout-padding bg-[url('/images/gradient-background.png')] bg-cover bg-center">
       <div className="xsm:pt-22 flex pb-14 pt-[70px] xsm:pt-20 sm:pb-20 sm:pt-24 md:pt-28">
         <div className="flex flex-1 flex-col pt-4">
-          <div className="text-3xl font-bold leading-normal xsm:text-4xl sm:text-[42px] lg:text-5xl">
+          <div className="text-center text-3xl font-bold leading-normal xsm:text-4xl sm:text-[42px] md:text-left lg:text-5xl">
             <span>Start your</span>
             <span className="bg-gradient-to-r from-sky-400 to-pink-400 bg-clip-text text-transparent">
               &nbsp;coding
             </span>
           </div>
-          <span className="mb-6 mt-0 text-3xl font-bold leading-normal xsm:mt-3 xsm:text-4xl sm:mb-10 sm:text-[42px] lg:text-5xl">
+          <span className="mb-6 mt-0 text-center text-3xl font-bold leading-normal xsm:mt-3 xsm:text-4xl sm:mb-10 sm:text-[42px] md:text-left lg:text-5xl">
             journey today
           </span>
-          <p className="mb-8 w-full text-sm leading-relaxed text-neutral_content xsm:w-3/4 sm:mb-14 sm:w-2/3 sm:text-base">
+          <div className="mb-8 w-full text-center text-sm leading-relaxed text-neutral_content sm:mb-10 sm:text-base md:mb-14 md:w-2/3 md:text-left">
             Hustcoder is the best platform to help you sharpen your
             problem-solving skills for coding interviews and build a strong
             foundation for your software development career.
-          </p>
-          <Link href="/#" className="landing-button">
-            Create account
-          </Link>
+          </div>
+          <div className="flex w-full items-center justify-center md:block">
+            <Link href="/#" className="landing-button">
+              Create account
+            </Link>
+          </div>
         </div>
         <div className="hidden lg:block">
           <Image
